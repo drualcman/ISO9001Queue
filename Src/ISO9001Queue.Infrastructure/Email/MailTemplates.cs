99ab76e9beee4e1a29fdf5dab72ef76f4f668eaa
Sergi Ortiz Gómez <drualcman@msn.com>
2026-08-23 19:59:01 +0800

    Add thai lang

--- Src/ISO9001Queue.Infrastructure/Email/MailTemplates.cs
+++ Src/ISO9001Queue.Infrastructure/Email/MailTemplates.cs
@@ -19,6 +19,7 @@ internal static class MailTemplates
         if (code.StartsWith("fr")) return ("fr", CultureInfo.GetCultureInfo("fr"));
         if (code.StartsWith("ko")) return ("ko", CultureInfo.GetCultureInfo("ko"));
         if (code.StartsWith("it")) return ("it", CultureInfo.GetCultureInfo("it"));
+        if (code.StartsWith("th")) return ("th", CultureInfo.GetCultureInfo("th"));
         return ("en", CultureInfo.InvariantCulture);
     }
 
